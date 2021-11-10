89551e97a667935a7cf9c17678f9df3a1f49136d
junejaabnv <94040858+junejaabnv@users.noreply.github.com>
2021-11-10 13:41:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var path = require('path');
 var bodyParser = require('body-parser')
 
 
-var server = app.listen(3001, function () {
+var server = app.listen(process.env.PORT || 3001, function () {
     var host = server.address().address
     var port = server.address().port
     console.log("Example app listening at http://%s:%s", host, port)
